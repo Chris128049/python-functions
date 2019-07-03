45c06450555ab25bd66e669be540d9df76e552e7
Chris Nava <chris128049@gmail.com>
2019-07-02 17:36:39 -0700

    first commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+empty_list = []
+ninjas = ['Rozen', 'KB', 'Oliver']
+print(ninjas[2]) 	# output: Oliver
+ninjas[0] = 'Francis'
+ninjas.append('Michael')
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver', 'Michael']
+ninjas.pop()
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver']
+ninjas.pop(1)
+print(ninjas)	# output: ['Francis', 'Oliver']
